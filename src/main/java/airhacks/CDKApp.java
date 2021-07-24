033cdabc531de6ea4242356528ce3115deb60930
Adam Bien <abien@adam-bien.com>
2021-07-24 04:59:27 +0200

    app name extracted into a new variable

--- src/main/java/airhacks/CDKApp.java
+++ src/main/java/airhacks/CDKApp.java
@@ -5,14 +5,16 @@ import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.Tags;
 
-import java.util.Arrays;
+
 
 public class CDKApp {
     public static void main(final String[] args) {
+        
             var app = new App();
+            var appName = "aws-cdk-plain";
             Tags.of(app).add("project", "airhacks.live");
             Tags.of(app).add("environment","workshops");
-            Tags.of(app).add("application", "aws-cdk-plain");
+            Tags.of(app).add("application", appName);
 
         var environment = Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
