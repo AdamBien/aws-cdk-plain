ca1dde6d9881d9667abaf6497f8cbb5e0a1fa4a8
Adam Bien <abien@adam-bien.com>
2025-04-26 06:58:57 +0200

    jsonb dependency removed

--- src/main/java/airhacks/CDKApp.java
+++ src/main/java/airhacks/CDKApp.java
@@ -1,29 +1,22 @@
 package airhacks;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import jakarta.json.bind.JsonbBuilder;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.Tags;
 
-
-
 public class CDKApp {
-    
+
     public static void main(final String[] args) {
 
-            var app = new App();
-            var appName = "aws-cdk-plain";
-            Tags.of(app).add("project", "airhacks.live");
-            Tags.of(app).add("environment","workshops");
-            Tags.of(app).add("application", appName);
+        var app = new App();
+        var appName = "aws-cdk-plain";
+        Tags.of(app).add("project", "airhacks.live");
+        Tags.of(app).add("environment", "workshops");
+        Tags.of(app).add("application", appName);
 
-            var stackProps = StackProps.builder()
-                    .build();
-            var configuration = Configuration.load();
+        var stackProps = StackProps
+                .builder()
+                .build();
         new CDKStack(app, appName, stackProps);
         app.synth();
     }
